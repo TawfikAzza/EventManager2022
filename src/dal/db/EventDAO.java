376bdef365c43bfc56c386723a705a71a452d700
TawfikAzza <tawf0021@easv365.dk>
2022-03-14 18:22:13 +0100

    First Upload

---
+++ src/dal/db/EventDAO.java
@@ -0,0 +1,33 @@
+package dal.db;
+
+import be.Events;
+import dal.interfaces.IEventDAO;
+
+import java.util.List;
+
+public class EventDAO implements IEventDAO {
+    @Override
+    public Events addEvent(Events event) {
+        return null;
+    }
+
+    @Override
+    public void removeEvent(Events event) {
+
+    }
+
+    @Override
+    public void updateEvent(Events event) {
+
+    }
+
+    @Override
+    public List<Events> getAllEvents() {
+        return null;
+    }
+
+    @Override
+    public Events getEvent(int id) {
+        return null;
+    }
+}
--- src/dal/interfaces/IEventDAO.java
+++ src/dal/interfaces/IEventDAO.java
@@ -1,4 +1,13 @@
 package dal.interfaces;
 
+import be.Events;
+
+import java.util.List;
+
 public interface IEventDAO {
+    public Events addEvent(Events event);
+    public void removeEvent(Events event);
+    public void updateEvent(Events event);
+    public List<Events> getAllEvents();
+    public Events getEvent(int id);
 }
