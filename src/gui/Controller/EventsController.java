7ee097b339d6fdf343849a15e476b92bd6ac9341
TawfikAzza <tawf0021@easv365.dk>
2022-03-15 08:33:43 +0100

    Implementing Events management

---
+++ src/gui/Controller/EventsController.java
@@ -0,0 +1,44 @@
+package gui.Controller;
+
+import be.Events;
+import bll.exception.EventManagerDAOException;
+import gui.Model.CoordinatorModel;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EventsController implements Initializable {
+
+    @FXML
+    private TableColumn<Events, String> eventName;
+    @FXML
+    private TableColumn<Events, String> numParticipants;
+    @FXML
+    private TableView<Events> tableEvents;
+    @FXML
+    private TableColumn<Events, String> eventDate;
+
+    private CoordinatorModel coordinatorModel;
+    public EventsController() {
+        coordinatorModel = new CoordinatorModel();
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private void updateTableView() {
+        eventName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        eventDate.setCellValueFactory(new PropertyValueFactory<>("strStartDate"));
+        try {
+            tableEvents.getItems().addAll(coordinatorModel.getAllEvents());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/gui/Model/CoordinatorModel.java
+++ src/gui/Model/CoordinatorModel.java
@@ -1,5 +1,21 @@
 package gui.Model;
 
+import be.Events;
+import bll.EventManager;
+import bll.EventManagerFacade;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class CoordinatorModel {
 
+    private ObservableList<Events> allEvents = FXCollections.observableArrayList();
+    private EventManagerFacade eventManagerFacade;
+    public CoordinatorModel() {
+        eventManagerFacade = new EventManager();
+    }
+    public ObservableList<Events> getAllEvents() throws Exception {
+        allEvents.clear();
+        allEvents.addAll(eventManagerFacade.getAllEvents());
+        return allEvents;
+    }
 }
