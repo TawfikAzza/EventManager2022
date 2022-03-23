cd085882e189d0eb17f11b19f4b679bea1cb7e9a
TawfikAzza <tawf0021@easv365.dk>
2022-03-23 10:31:28 +0100

    Start the work on the ticket

--- src/gui/Controller/TicketParticipantController.java
+++ src/gui/Controller/TicketParticipantController.java
@@ -1,4 +1,29 @@
 package gui.Controller;
 
+import be.Events;
+import be.Participant;
+import be.Ticket;
+import be.TicketType;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.TextFlow;
+
 public class TicketParticipantController {
+
+    @FXML
+    private ImageView imageQRCode;
+
+    @FXML
+    private TextFlow lblDescription;
+
+    @FXML
+    private Label lblEventName,lblLocation,lblName,lblTicketType;
+
+
+    private TicketType ticketType;
+    private Participant participant;
+    private Events event;
+
+
 }
